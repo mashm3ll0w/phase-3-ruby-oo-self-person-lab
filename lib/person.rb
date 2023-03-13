49294e992d10668596cd6ac636d4367671e0422f
Charles Swaleh <charlswall@live.com>
2023-03-13 14:15:16 +0300

    Completed the initialization and attributes of the Person Class

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,39 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(value)
+        if value > 10
+            @happiness = 10
+        elsif value < 0
+            @happiness = 0
+        else
+            @happiness = value
+        end
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def hygiene=(value)
+        if value > 10
+            @hygiene = 10
+        elsif value < 0
+            @hygiene = 0
+        else
+            @hygiene = value
+        end
+    end
+
+    def hygiene
+        @hygiene
+    end
+end
